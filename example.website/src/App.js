381403c96550a0104d1735fd83ff20ae6a0ce732
Chris Sheehan <chris.sheehan@ziglu.io>
2022-12-22 15:16:36 +0000

    example api call

--- example.website/src/App.js
+++ example.website/src/App.js
@@ -1,13 +1,42 @@
+import React, { useEffect, useState } from "react"
+
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [users, setUsers] = useState([])
+
+  const fetchData = () => {
+
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => {
+        return response.json()
+      })
+      .then(data => {
+        setUsers(data)
+      })
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          <div>
+            {users.length > 0 && (
+              <ul>
+                {users.map(user => (
+                  <li key={user.id}>{user.name}</li>
+                ))}
+              </ul>
+            )}
+          </div>
         </p>
         <a
           className="App-link"
